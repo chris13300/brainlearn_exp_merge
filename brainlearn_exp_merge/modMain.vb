cd4c0a824b8345e43d30f404586e277f70510977
chris13300 <54830918+chris13300@users.noreply.github.com>
2022-11-11 17:19:26 +0100

    Update modMain.vb

--- brainlearn_exp_merge/modMain.vb
+++ brainlearn_exp_merge/modMain.vb
@@ -23,4 +23,83 @@
         End If
     End Sub
 
+    Public Function defragBIN(cheminBIN As String, profMin As Integer) As String
+        Dim tabBIN(0) As Byte, i As Long, tabNEW() As Byte, offset As Long
+        Dim tabTampon(23) As Byte, compteur As Integer, nbSuppression As Integer
+        Dim message As String, pos As Long
+        Dim lectureBIN As IO.FileStream, posLecture As Long, tailleBIN As Long, tailleTampon As Long, reservation As Boolean
+
+        message = ""
+
+        If My.Computer.FileSystem.FileExists(cheminBIN & ".bak") Then
+            My.Computer.FileSystem.DeleteFile(cheminBIN & ".bak")
+        End If
+
+        posLecture = 0
+        tailleBIN = FileLen(cheminBIN)
+        tailleTampon = tailleBIN
+        i = 50
+        lectureBIN = New IO.FileStream(cheminBIN, IO.FileMode.Open, IO.FileAccess.Read, IO.FileShare.ReadWrite)
+
+        compteur = 0
+        nbSuppression = 0
+
+        While posLecture < tailleBIN
+            If posLecture + tailleTampon <= tailleBIN Then
+                reservation = False
+                Do
+                    Try
+                        ReDim tabBIN(tailleTampon - 1)
+                        reservation = True
+                    Catch ex As Exception
+                        i = i - 1
+                        tailleTampon = 24 * i * 1000000
+                    End Try
+                Loop Until reservation
+                lectureBIN.Read(tabBIN, 0, tabBIN.Length)
+            Else
+                tailleTampon = tailleBIN - posLecture
+                ReDim tabBIN(tailleTampon - 1)
+                lectureBIN.Read(tabBIN, 0, tabBIN.Length)
+            End If
+
+            ReDim tabNEW(UBound(tabBIN))
+            pos = 0
+            offset = 0
+            Do
+                Array.Copy(tabBIN, pos, tabTampon, 0, 24)
+                If profMin <= tabTampon(8) Then
+                    Array.Copy(tabTampon, 0, tabNEW, offset * 24, 24)
+                    offset = offset + 1
+                Else
+                    nbSuppression = nbSuppression + 1
+                End If
+
+                compteur = compteur + 1
+                pos = pos + 24
+            Loop While pos < tabBIN.Length
+
+            tabBIN = Nothing
+
+            ReDim Preserve tabNEW(offset * 24 - 1)
+            My.Computer.FileSystem.WriteAllBytes(cheminBIN & ".bak", tabNEW, True)
+
+            tabNEW = Nothing
+
+            posLecture = lectureBIN.Position
+        End While
+
+        lectureBIN.Close()
+
+        message = "info string " & nomFichier(cheminBIN) & " -> Total moves: " & compteur & ". Empty moves: " & nbSuppression & ". Fragmentation: " & Format(nbSuppression / compteur, "0.00%") & vbCrLf
+        message = message & "info string Saved " & Format(compteur - nbSuppression, "0 moves") & " to " & nomFichier(cheminBIN) & " file"
+
+        If compteur > nbSuppression Then
+            My.Computer.FileSystem.DeleteFile(cheminBIN)
+            My.Computer.FileSystem.RenameFile(cheminBIN & ".bak", nomFichier(cheminBIN))
+        End If
+
+        Return message
+    End Function
+
 End Module
